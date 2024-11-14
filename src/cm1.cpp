ac8f3d62893a0ba5d4048e99b43d461ccae5bcfe
kazrin <kazunori.yoshimura1988@gmail.com>
2024-11-14 17:29:42 +0900

    Fix

--- src/cm1.cpp
+++ src/cm1.cpp
@@ -2,17 +2,37 @@
 #include <WiFi.h>
 #include <WiFiConfig.h>
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  WiFi.begin(WiFiConf::ssid, WiFiConf::pass);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+  delay(5000);
 
+  if (WiFiConf::ssid)
+  {
+    if (WiFiConf::pass != "")
+    {
+      Serial.println("Connecting to WiFi with SSID and password");
+      WiFi.begin(WiFiConf::ssid, WiFiConf::pass);
+    }
+    else
+    {
+      Serial.println("Connecting to WiFi with SSID");
+      WiFi.begin(WiFiConf::ssid);
+    }
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      delay(500);
+      Serial.print(".");
+    }
+  }
+  else
+  {
+    Serial.println("No WiFi credentials found");
+  }
 }
 
-void loop() {
+void loop()
+{
   Serial.println("This is cm1.cpp");
   delay(1000);
 }
